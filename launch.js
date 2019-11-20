082fb3474ff36c97b9c5c20c9ea846fe2bcc9a60
Peter Walker <peterw@suprafax.net>
2019-11-20 09:46:52 -0800

    transform SMTP settings

--- launch.js
+++ launch.js
@@ -26,4 +26,14 @@ process.env.database__connection__database = databaseUrl.pathname.replace('/', '
 process.env.server__port = process.env.PORT
 process.env.server__host = '0.0.0.0'
 
+// Transform some SMTP settings.  This example is for mailgun
+process.env.mail__transport = 'SMTP';
+process.env.mail__options__service = process.env.SMTP_SERVICE;
+process.env.mail__options__host = process.env.SMTP_HOST;
+process.env.mail__options__port = process.env.SMTP_PORT;
+process.env.mail__options__secureConnection = process.env.SMTP_SECURE || false;
+process.env.mail__options__auth__user = process.env.SMTP_USERNAME;
+process.env.mail__options__auth__pass = process.env.SMTP_PASSWORD;
+
+// This forked copy is the the only way I could figure out how to make the server get all the environment variables we've set above^
 const child = fork('server.js');
